5e78adfa0996f700ac70504727077be4beaa6851
d_liyanage <admliyanage@gmail.com>
2020-10-28 22:39:58 +0530

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <h1>Dilshanka</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
