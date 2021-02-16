284c06a2cc8f06c3b24c442c4386aab3853f6cda
d_liyanage <admliyanage@gmail.com>
2021-02-17 00:05:39 +0530

    tailwindcss added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <Main/>
+   Hello
   </div>
 </template>
 
 <script>
-import Main from './components/Main.vue'
+
 
 export default {
-  name: 'App',
   components: {
-    Main
+   
   }
 }
 </script>
